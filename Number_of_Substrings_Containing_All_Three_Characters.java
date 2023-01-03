57a092ac637b1e83e7c424480bfa3db66c76e57e
Aarif <03aarif20@gmail.com>
2023-01-03 19:00:09 +0530

    Add files via upload

---
+++ Number_of_Substrings_Containing_All_Three_Characters.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Number_of_Substrings_Containing_All_Three_Characters {
+    public static int numberOfSubstings(String s){
+        int freq[] =new int[3];
+        int l=0;
+        int m=0;
+        int res=0;
+        for(m=0;m<s.length();m++){
+            freq[s.charAt(m)-'a']++;
+            while(freq[0]>0&&freq[1]>0&&freq[2]>0){
+                freq[s.charAt(l)-'a']--;
+                System.out.println(m);
+                res+=s.length()-m;
+                l++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        String s ="aaacb";
+        numberOfSubstings(s);        
+    }
+    
+}
