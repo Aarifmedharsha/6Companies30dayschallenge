49af857c531eb154cb989be7d821ce620f194166
Aarif <03aarif20@gmail.com>
2023-01-03 12:04:50 +0530

    Update Minimum_Deletions_to_Make_Array_Divisible.java

--- Minimum_Deletions_to_Make_Array_Divisible.java
+++ Minimum_Deletions_to_Make_Array_Divisible.java
@@ -11,19 +11,19 @@ public class Minimum_Deletions_to_Make_Array_Divisible {
             }
             return temp;
         }
-        public static int minDelete(int arr1[],int arr2[]){
-            int count=0;
-            int g =gcdofarray(arr2);
-            Arrays.sort(arr1);
-            for(int i=0;i<arr1.length;i++){
-                if(g%arr1[i]==0) return count;
+    public int minOperations(int[] nums, int[] numsDivide) {
+        int count=0;
+        int g =gcdofarray(numsDivide);
+        Arrays.sort(nums);
+            for(int i=0;i<nums.length;i++){
+                if(g%nums[i]==0) return count;
                 else count++;
             }
             return -1;
-        }
+    }
         public static void main(String[] args) {
             int arr1[] ={2,2,3,3,4};
             int arr2[] ={9,6,15,18,21,3};
-            System.out.println(minDelete(arr1, arr2));
+            System.out.println(minOperations(arr1, arr2));
         }
 }
