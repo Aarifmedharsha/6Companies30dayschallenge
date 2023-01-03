ab03174dd03e42aaaab6b8624e10dc95a4aac9e4
Aarif <03aarif20@gmail.com>
2023-01-03 09:51:50 +0530

    Update and rename airplaneseat.java to Airplane_Seat_Assignment_Probability.java

--- Airplane_Seat_Assignment_Probability.java
+++ Airplane_Seat_Assignment_Probability.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;   
-public class airplaneseat {
+public class Airplane_Seat_Assignment_Probability{
     public static float seat(int n){
         if(n==0||n==1) return 1;
         else return 1/2;
