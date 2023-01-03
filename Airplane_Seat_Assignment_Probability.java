44701e9800fe4614cb1883a7984e5f17cd71d9e2
Aarif <03aarif20@gmail.com>
2023-01-03 12:00:19 +0530

    Update Airplane_Seat_Assignment_Probability.java

--- Airplane_Seat_Assignment_Probability.java
+++ Airplane_Seat_Assignment_Probability.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;   
 public class Airplane_Seat_Assignment_Probability{
-    public static float seat(int n){
+    public static double nthPersonGetsNthSeat(int n){
         if(n<=1) return 1;
         else return 0.50000;
     }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println(seat(n));
+        System.out.println(nthPersonGetsNthSeat(n));
     }
 }
