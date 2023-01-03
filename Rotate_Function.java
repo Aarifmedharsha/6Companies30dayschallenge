7340b6e2cb6601211d72d9a1f30ab704cbd618f3
Aarif <03aarif20@gmail.com>
2023-01-03 14:15:58 +0530

    Add files via upload

---
+++ Rotate_Function.java
@@ -0,0 +1,21 @@
+public class Rotate_Function {
+    public static int maxRotateFunction(int[] nums) {
+        int temp=0;
+        int total=0;
+        for(int i=0;i<nums.length;i++){
+            temp+=i*(nums[i]);
+            total+=nums[i];
+        }
+        int max=temp;
+        for(int i=nums.length-1;i>=1;i--){
+            temp=temp+total-(nums.length*nums[i]);
+            max=Math.max(temp,max);
+        }
+        return max;
+    }
+    }
+    public static void main(String[] args) {
+        int nums[] ={4,3,2,6};
+        System.out.println(maxRotateFunction(nums));
+    }
+}
