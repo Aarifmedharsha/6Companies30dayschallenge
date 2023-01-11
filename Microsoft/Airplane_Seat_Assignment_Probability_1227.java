76d95e62d4e593823fa3171e5966da725d29cc50
Aarif <03aarif20@gmail.com>
2023-01-11 14:05:18 +0530

    Update Airplane_Seat_Assignment_Probability_1227.java

--- Microsoft/Airplane_Seat_Assignment_Probability_1227.java
+++ Microsoft/Airplane_Seat_Assignment_Probability_1227.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;   
-public class Airplane_Seat_Assignment_Probability_1227{
-    public static double nthPersonGetsNthSeat(int n){
-        if(n<=1) return 1;
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        if(n==1) return 1;
         else return 0.50000;
-    }
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(nthPersonGetsNthSeat(n));
     }
 }
