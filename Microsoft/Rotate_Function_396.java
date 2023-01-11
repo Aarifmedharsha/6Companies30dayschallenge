f69376c077d1b1b85285a011aa04377cc5868fa5
Aarif <03aarif20@gmail.com>
2023-01-11 14:04:22 +0530

    Update Rotate_Function_396.java

--- Microsoft/Rotate_Function_396.java
+++ Microsoft/Rotate_Function_396.java
@@ -1,21 +1,16 @@
-public class Rotate_Function_396 {
-    public static int maxRotateFunction(int[] nums) {
+class Solution {
+    public int maxRotateFunction(int[] nums) {
         int temp=0;
-        int total=0;
+        int sum=0;
         for(int i=0;i<nums.length;i++){
-            temp+=i*(nums[i]);
-            total+=nums[i];
+            temp+=i*nums[i];
+            sum+=nums[i];
         }
         int max=temp;
         for(int i=nums.length-1;i>=1;i--){
-            temp=temp+total-(nums.length*nums[i]);
-            max=Math.max(temp,max);
+            temp+=sum-(nums.length*nums[i]);
+            max=Math.max(max,temp);
         }
         return max;
-    }
-    }
-    public static void main(String[] args) {
-        int nums[] ={4,3,2,6};
-        System.out.println(maxRotateFunction(nums));
     }
 }
