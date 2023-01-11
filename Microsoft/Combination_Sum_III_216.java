3397f7a3b3a6f96248cc5f3f2c992621b4656d46
Aarif <03aarif20@gmail.com>
2023-01-11 14:10:48 +0530

    Update Combination_Sum_III_216.java

--- Microsoft/Combination_Sum_III_216.java
+++ Microsoft/Combination_Sum_III_216.java
@@ -1,12 +1,11 @@
-import java.util.*;
-public class Combination_Sum_III_216 {
-    public static List<List<Integer>> combinationSum3(int k, int n) {
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> collectionofsubsets=new ArrayList<>();
         List<Integer> currsubset=new ArrayList<>();
         comb(1,9,k,n,0,collectionofsubsets,currsubset);
         return collectionofsubsets;
     }
-    static void comb(int s,int e,int k,int n,int sum,List<List<Integer>> collectionofsubsets,List<Integer> currsubset){
+    void comb(int s,int e,int k,int n,int sum,List<List<Integer>> collectionofsubsets,List<Integer> currsubset){
         if(k==0){
             if(sum==n){
                 collectionofsubsets.add(new ArrayList(currsubset));
@@ -22,11 +21,6 @@ public class Combination_Sum_III_216 {
         sum-=s;
         currsubset.remove(currsubset.size()-1);
         comb(s+1,e,k,n,sum,collectionofsubsets,currsubset);
-    }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int k=sc.nextInt();
-        int n=sc.nextInt();
-        System.out.println(combinationSum3(k,n));
+
     }
 }
